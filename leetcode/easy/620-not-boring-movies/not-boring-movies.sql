119db72fb9afda4900199472885e8a7a433419bd
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-09 23:14:18 +0530

    Time: 515 ms (17.28%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/easy/620-not-boring-movies/not-boring-movies.sql
+++ leetcode/easy/620-not-boring-movies/not-boring-movies.sql
@@ -1,4 +1,8 @@
 SELECT id, movie, description, rating
 FROM Cinema
-WHERE id % 2 <> 0 and description <> 'boring'
+WHERE id % 2 = 1 and description <> 'boring'
 ORDER BY 4 DESC
+
+
+# another way : 
+# WHERE MOD(id, 2) = 1
