eae5570901dd2a5cb70a969a214ed02063bb2a26
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-10 15:36:35 +0530

    Time: 4954 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/easy/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ leetcode/easy/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,4 +1,7 @@
-SELECT contest_id, ROUND(COUNT(DISTINCT user_id)/ (SELECT COUNT(user_id) FROM Users)* 100,2) as percentage
+SELECT contest_id, ROUND(COUNT(user_id)/ (SELECT COUNT(user_id) FROM Users)* 100,2) as percentage
 FROM Register
 GROUP BY 1
 ORDER BY 2 DESC, 1
+
+
+# moving "* 100" outside decreases the runtime --> good thing.
