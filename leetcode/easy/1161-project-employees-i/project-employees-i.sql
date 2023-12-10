c597ecdc7425273a9af83594841aed5142b1917c
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-10 15:24:10 +0530

    Time: 1047 ms (32.06%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/easy/1161-project-employees-i/project-employees-i.sql
+++ leetcode/easy/1161-project-employees-i/project-employees-i.sql
@@ -1,3 +1,7 @@
-SELECT project_id, ROUND(AVG(experience_years), 2) as average_years
+SELECT p.project_id, ROUND(AVG(experience_years), 2) as average_years
 FROM Project p LEFT JOIN Employee e ON p.employee_id = e.employee_id
 GROUP BY project_id
+
+
+# IFNULL not needed due to this statement in the description : 
+# --> It's guaranteed that experience_years is not NULL.
