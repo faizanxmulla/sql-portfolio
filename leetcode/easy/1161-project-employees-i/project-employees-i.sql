216697e838e9751dd78125b37dfcb06c93a19eaa
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-10 15:22:04 +0530

    Time: 968 ms (47.87%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/easy/1161-project-employees-i/project-employees-i.sql
+++ leetcode/easy/1161-project-employees-i/project-employees-i.sql
@@ -1,3 +1,3 @@
-SELECT project_id, IFNULL(ROUND(AVG(experience_years), 2), 0) as average_years
+SELECT project_id, ROUND(AVG(experience_years), 2) as average_years
 FROM Project p LEFT JOIN Employee e ON p.employee_id = e.employee_id
 GROUP BY project_id
