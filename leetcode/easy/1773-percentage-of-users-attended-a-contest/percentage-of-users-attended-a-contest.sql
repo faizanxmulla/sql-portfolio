fcf0a2d01d400c4aad0ac2752c2eb5d8655a56e8
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-10 15:35:27 +0530

    Time: 1749 ms (46.56%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/easy/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ leetcode/easy/1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -1,5 +1,4 @@
-SELECT contest_id, ROUND(COUNT(DISTINCT user_id) *100 / (SELECT COUNT(user_id) FROM Users),2) as percentage
+SELECT contest_id, ROUND(COUNT(DISTINCT user_id)/ (SELECT COUNT(user_id) FROM Users)* 100,2) as percentage
 FROM Register
 GROUP BY 1
 ORDER BY 2 DESC, 1
-
