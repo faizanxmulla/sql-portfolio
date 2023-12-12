1bb649584f2f8f2037cdaf1462cbeeab42fee210
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-12 15:56:42 +0530

    Time: 4471 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode/easy/1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
@@ -0,0 +1,29 @@
+SELECT DISTINCT v.customer_id, COUNT(v.visit_id) as count_no_trans
+FROM Visits v LEFT JOIN Transactions t ON t.visit_id = v.visit_id 
+WHERE t.transaction_id IS NULL
+GROUP BY 1
+
+
+# alternate WHERE condition --> WHERE t.amount IS NULL 
+
+# alternate solutions : 
+
+# 1. Subquery : "NOT EXISTS"
+
+# SELECT customer_id, COUNT(visit_id) as count_no_trans 
+# FROM Visits v
+# WHERE NOT EXISTS (
+# 	SELECT visit_id FROM Transactions t 
+# 	WHERE t.visit_id = v.visit_id
+# 	)
+# GROUP BY customer_id
+
+
+# 2. Subquery : "NOT IN"
+
+# SELECT customer_id, COUNT(visit_id) as count_no_trans 
+# FROM Visits
+# WHERE visit_id NOT IN (
+# 	SELECT visit_id FROM Transactions
+# 	)
+# GROUP BY customer_id
