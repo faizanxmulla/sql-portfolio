4ec80a190e4a1ba215349828488b94e1317c79c9
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-15 16:00:52 +0530

    Time: 1232 ms (88.00%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/easy/196-delete-duplicate-emails/delete-duplicate-emails.sql
+++ leetcode/easy/196-delete-duplicate-emails/delete-duplicate-emails.sql
@@ -10,3 +10,13 @@ WHERE p1.Email = p2.Email and p1.Id < p2.Id
 # WHERE id NOT IN (select min(id) as id 
 #                  from Person 
 #                  group by email)
+
+
+# Solution using WINDOW function : (much FASTER)
+
+# DELETE FROM Person
+# WHERE id IN (SELECT id
+#             FROM  
+#               (SELECT id,  ROW_NUMBER() OVER(PARTITION BY email ORDER BY id) row_num
+#               FROM Person) sq 
+#             WHERE row_num > 1)
