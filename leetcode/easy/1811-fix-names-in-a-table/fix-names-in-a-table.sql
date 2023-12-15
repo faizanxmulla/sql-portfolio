8df5bc4b8e53b1afc1713fd1d310574d5517389f
Faizan Mulla <faizanamulla69@gmail.com>
2023-12-15 12:33:19 +0530

    Time: 1081 ms (71.79%) | Memory: 0B (100.00%) - LeetSync

---
+++ leetcode/easy/1811-fix-names-in-a-table/fix-names-in-a-table.sql
@@ -0,0 +1,6 @@
+SELECT user_id, CONCAT(UCASE(SUBSTR(name, 1,1)), LCASE(SUBSTR(name, 2, length(name)))) as name
+FROM Users 
+ORDER BY 1
+
+#  instead of : {UCASE and LCASE} --> could have used {UPPER & LOWER}.
+#  instead of : {SUBSTR} --> could have used {SUBSTRING} or {MID} or{LEFT & RIGHT}.
