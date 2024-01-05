3f947db161dfe65f6298baf8e72fe0ea049cc0b7
Faizan Mulla <faizanamulla69@gmail.com>
2024-01-05 16:52:29 +0530

    Time: 1634 ms (8.90%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/medium/1317-monthly-transactions-i/monthly-transactions-i.sql
+++ leetcode/medium/1317-monthly-transactions-i/monthly-transactions-i.sql
@@ -9,10 +9,12 @@ FROM Transactions
 GROUP BY 1, 2
 
 
-# can extract month usnig the follwing methods : 
+# can extract month usnig the follwing methods : (2nd and 4th --> good)
 
 # 1. LEFT(trans_date, 7)
 # 2. DATE_FORMAT(trans_date, '%Y-%m')
+# 3. CONCAT(Year(trans_date),'-',LPAD(Month(trans_date),2,'0'))
+# 4. SUBSTRING(trans_date,1,7)
 
 # also instead of IF, we can use {CASE, WHEN, THEN, ELSE, END}. 
 
