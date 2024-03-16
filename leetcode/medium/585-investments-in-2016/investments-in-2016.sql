bd163485a002e39387a836363d976c6c24ce55b9
Faizan Mulla <faizanamulla69@gmail.com>
2024-03-16 19:50:16 +0530

    update :  Medium/investments-in-2016.sql
    add another approach to solving the question using WINDOW function.

--- leetcode/medium/585-investments-in-2016/investments-in-2016.sql
+++ leetcode/medium/585-investments-in-2016/investments-in-2016.sql
@@ -11,4 +11,15 @@ FROM   (SELECT tiv_2016
                                        HAVING   Count(*) > 1)) AS X; 
 
 
+-- other approaches: 
+
+SELECT round(sum(tiv_2016), 2) as tiv_2016
+FROM   (
+    SELECT *,
+           COUNT(*) OVER(PARTITION BY tiv_2015) as a,
+           COUNT(*) OVER(PARTITION BY lat, lon) as b
+    FROM   Insurance
+) as x
+WHERE   a>=2 and b=1
+
 -- remarks: exact same question in the "Ninjas" section of coding ninjas.
