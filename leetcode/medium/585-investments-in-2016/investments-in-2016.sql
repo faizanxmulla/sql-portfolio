0b6502b80b23e722716523cef18f2788204150c0
Faizan Mulla <faizanamulla69@gmail.com>
2024-05-26 18:05:50 +0530

    Time: 462 ms (78.99%) | Memory: 0B (100.00%) - LeetSync

--- leetcode/medium/585-investments-in-2016/investments-in-2016.sql
+++ leetcode/medium/585-investments-in-2016/investments-in-2016.sql
@@ -1,25 +1,15 @@
-SELECT Round(Sum(tiv_2016), 2) AS TIV_2016
-FROM   (SELECT tiv_2016
-        FROM   insurance
-        WHERE  tiv_2015 IN (SELECT tiv_2015
-                            FROM   insurance
-                            GROUP  BY tiv_2015
-                            HAVING Count(*) > 1)
-               AND (lat, lon) NOT IN (SELECT    lat,lon
-                                       FROM     insurance
-                                       GROUP BY 1, 2
-                                       HAVING   Count(*) > 1)) AS X; 
-
-
--- other approaches: 
-
-SELECT round(sum(tiv_2016), 2) as tiv_2016
-FROM   (
-    SELECT *,
+WITH CTE AS (
+    SELECT *, 
            COUNT(*) OVER(PARTITION BY tiv_2015) as a,
            COUNT(*) OVER(PARTITION BY lat, lon) as b
     FROM   Insurance
-) as x
-WHERE   a>=2 and b=1
+)
+SELECT ROUND(SUM(tiv_2016)::NUMERIC, 2) as tiv_2016
+FROM   CTE
+WHERE  a>=2 AND b=1
 
--- remarks: exact same question in the "Ninjas" section of coding ninjas.
+
+-- SELECT   tiv_2015, lat, lon
+-- FROM     Insurance
+-- GROUP BY 1, 2, 3
+-- HAVING   COUNT(tiv_2015) >= 2 
