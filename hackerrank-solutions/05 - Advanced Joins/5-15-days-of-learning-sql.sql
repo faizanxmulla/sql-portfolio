6f6fa40730e42933aa9f62a9a66965469aa42b9f
Faizan Mulla <faizanamulla69@gmail.com>
2024-09-11 22:25:47 +0530

    solve: question #5

--- hackerrank-solutions/05 - Advanced Joins/5-15-days-of-learning-sql.sql
+++ hackerrank-solutions/05 - Advanced Joins/5-15-days-of-learning-sql.sql
@@ -7,11 +7,34 @@
 
 
 
+-- Solution: 
+
+
+WITH submissions_ranked as (
+	SELECT submission_date, hacker_id, COUNT(*) AS submissions_count, DENSE_RANK() OVER(ORDER BY submission_date) as day_number
+	FROM   submissions
+	GROUP BY 1, 2
+),
+cumulative_submissions as (
+	SELECT   *, 
+	         COUNT(*) OVER(PARTITION BY hacker_id ORDER BY submission_date) as till_date_submissions, 
+	         CASE WHEN day_number=COUNT(*) OVER(PARTITION BY hacker_id ORDER BY submission_date) THEN 1 ELSE 0 END as flag
+	FROM     submissions_ranked
+	ORDER BY 1, 2
+),
+ranked_submissions_per_day as (
+	SELECT *, 
+		   SUM(flag) OVER(PARTITION BY submission_date) AS unique_submissions, 
+		   ROW_NUMBER() OVER(PARTITION BY submission_date ORDER BY submissions_count desc, hacker_id) as rn
+	FROM   cumulative_submissions 
+)
+SELECT submission_date, unique_submissions, hacker_id, name as hacker_name
+FROM   ranked_submissions_per_day rs JOIN hackers h USING(hacker_id)
+WHERE  rn=1
 
 
 
-
-
+-- Ankit Bansal YT explanation link: https://www.youtube.com/watch?v=zrCIWGHnLao&list=PLBTZqjSKn0IeKBQDjLmzisazhqQy4iGkb&index=67
 
 
 
