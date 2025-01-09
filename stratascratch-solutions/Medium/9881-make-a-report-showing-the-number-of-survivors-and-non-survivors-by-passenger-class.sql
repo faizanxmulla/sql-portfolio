e5e2f70e934aedd8935583d3ab9bc61cd9e7edc7
Faizan Mulla <faizanamulla69@gmail.com>
2025-01-09 11:05:59 +0530

    solve: Medium #9881, #9882

---
+++ stratascratch-solutions/Medium/9881-make-a-report-showing-the-number-of-survivors-and-non-survivors-by-passenger-class.sql
@@ -0,0 +1,6 @@
+SELECT   survived,
+         SUM(CASE WHEN pclass = 1 THEN 1 ELSE 0 END) as first_class,
+         SUM(CASE WHEN pclass = 2 THEN 1 ELSE 0 END) as second_class,
+         SUM(CASE WHEN pclass = 3 THEN 1 ELSE 0 END) as third_class
+FROM     titanic
+GROUP BY survived
